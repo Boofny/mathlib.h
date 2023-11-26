2121fe2c358ed1dc0d93029b76efe647c782fb41
Danin Sadžak <daninsadzak@gmail.com>
2023-11-26 14:13:29 +0100

    Finished implementing add()

--- examples.c
+++ examples.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include "mathlib.h"
 
 int main(void) {
-    printf("Hello, world!\n");
+    printf("Examples of addition: %.2f", add(2, 3));
     return 0;
 }
--- mathlib.h
+++ mathlib.h
@@ -1,1 +1,5 @@
-#include <stdio.h>
+#pragma once
+
+float add(int num1, int num2) {
+    return num1 + num2;
+}
