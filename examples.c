ab844366b5de53ba5aab03f1cda77709013016aa
Danin Sadžak <daninsadzak@gmail.com>
2023-11-26 14:19:23 +0100

    Finished implementing multiply()

--- examples.c
+++ examples.c
@@ -2,7 +2,8 @@
 #include "mathlib.h"
 
 int main(void) {
-    printf("Example of addition: %.2f\n", add(2, 3));
-    printf("Example of subtraction: %.2f\n", subtract(6, 10));
+    printf("Example of addition:        %.2f\n", add(2, 3));
+    printf("Example of subtraction:     %.2f\n", subtract(6, 10));
+    printf("Example of multiplication:  %.2f\n", multiply(23, 11));
     return 0;
 }
--- mathlib.h
+++ mathlib.h
@@ -8,3 +8,7 @@ double add(double num1, double num2) {
 double subtract(double num1, double num2) {
     return num1 - num2;
 }
+
+double multiply(double num1, double num2) {
+    return num1 * num2;
+}
