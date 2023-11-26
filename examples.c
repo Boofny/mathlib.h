e330a635f79052647442d041a0f3b863b093353b
Danin Sadžak <daninsadzak@gmail.com>
2023-11-26 14:22:14 +0100

    Finished implementing divide()

--- examples.c
+++ examples.c
@@ -5,5 +5,6 @@ int main(void) {
     printf("Example of addition:        %.2f\n", add(2, 3));
     printf("Example of subtraction:     %.2f\n", subtract(6, 10));
     printf("Example of multiplication:  %.2f\n", multiply(23, 11));
+    printf("Example of division:        %.2f\n", divide(12, 2));
     return 0;
 }
