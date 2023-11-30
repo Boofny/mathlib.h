eef9dc8a50820eeee598206632556ccc979d07c8
Danin Sadžak <daninsadzak@gmail.com>
2023-11-30 20:06:17 +0100

    Finished implementing sineh() function

--- mathlib.h
+++ mathlib.h
@@ -98,7 +98,7 @@ double sinus(double radian) {
         }
     }
 
-    return fabsolute(result);
+    return result;
 } 
 
 double cosine(double radian) {
@@ -120,4 +120,16 @@ double tangent(double radian) {
 
 double cotangent(double radian) {
     return cosine(radian) / sinus(radian);
+}
+
+double sineh(double radian) {
+    double result = 0;
+    double copyRadian = radian;
+    double term;
+    for(int i = 0; i < 10; i++) {
+        term = (power(copyRadian, (i * 2 + 1))) / tgamma((i * 2 + 1) + 1);
+        result += term;
+    }
+
+    return result;
 }
